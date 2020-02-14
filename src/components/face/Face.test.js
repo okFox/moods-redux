87fb595c66f7b2b8667231bb5f6749f160fb3c2c
okFox <smayfield@gmail.com>
2020-02-13 17:22:28 -0800

    selector tests passing

--- src/components/face/Face.test.js
+++ src/components/face/Face.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Face from './Face';
+import { Provider } from 'react-redux';
+import store from '../../store';
 
 describe('Face component', () => {
   it('renders Face', () => {
-    const wrapper = shallow(<Face emoji="😀" />);
+    const wrapper = shallow(<Provider store={store}><Face emoji="😀" /></Provider>);
     expect(wrapper).toMatchSnapshot();
   });
 });
